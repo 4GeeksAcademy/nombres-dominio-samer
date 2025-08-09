53102bafbe969c3ece415eccb632c49a1760fbf3
samer <ysamer419@gmail.com>
2025-08-09 12:14:35 +0000

    generados los nombres

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-import "bootstrap";
-import "./style.css";
+     let pronoun = ['the', 'our'];
+        let adj = ['great', 'big'];
+        let noun = ['jogger', 'racoon'];
+        let currentIndex = 0;
+        let domainNames = [];
 
+      
+        for (let i = 0; i < pronoun.length; i++) {
+            for (let j = 0; j < adj.length; j++) {
+                for (let k = 0; k < noun.length; k++) {
+                    let domain = pronoun[i] + adj[j] + noun[k] + '.com';
+                    domainNames.push(domain);
+                }
+            }
+        }
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+        function generateDomains() {
+            if (currentIndex < domainNames.length) {
+                let domain = domainNames[currentIndex];
+                console.log(`${currentIndex + 1}. ${domain}`);
+                
+                let html = document.getElementById('results').innerHTML;
+                html += `<p>${currentIndex + 1}. ${domain}</p>`;
+                document.getElementById('results').innerHTML = html;
+                
+                currentIndex++;
+                
+                if (currentIndex === domainNames.length) {
+                    document.getElementById('results').innerHTML += `<p><strong>Total: ${domainNames.length} dominios generados</strong></p>`;
+                    console.log(`\nTotal de combinaciones generadas: ${domainNames.length}`);
+                }
+            }
+        }
